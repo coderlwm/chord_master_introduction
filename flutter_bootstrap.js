3a2c64fad7b1f3abf90432f9a5188f00eccaf90a
coderlwm <coderlwm@gmail.com>
2026-07-13 14:06:59 +0800

    new logo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3946934587" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4119949965" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
